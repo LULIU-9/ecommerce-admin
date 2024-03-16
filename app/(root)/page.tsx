ed6e6db0b061fd1e79ec2d03c27a968d7211771e
LULIU-9 <lu.liu4work@gmail.com>
2024-03-16 19:44:38 +1000

    feat: add modal store provider

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,19 @@
 "use client";
 
 import { Modal } from "@/components/ui/modal";
+import { useStoreModal } from "@/hooks/use-store-modal";
 import { UserButton } from "@clerk/nextjs";
+import { useEffect } from "react";
 
 export default function Home() {
-  return (
-    <div className="h-screen">
-      <Modal title="test" description="test des" isOpen onClose={() => {}} />
-    </div>
-  );
+  const isOpen = useStoreModal((state) => state.isOpen);
+  const onOpen = useStoreModal((state) => state.onOpen);
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
+
+  return <div className="h-screen">root page</div>;
 }
