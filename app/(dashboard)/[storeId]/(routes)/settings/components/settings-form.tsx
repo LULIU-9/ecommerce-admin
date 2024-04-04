0271fc9d1c47603a936234833e2fb345ac08ada2
LULIU-9 <lu.liu4work@gmail.com>
2024-04-04 22:50:11 +1000

    feat: add origin hook to prevent hydration issue

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -14,6 +14,7 @@ import {
 import Heading from "@/components/ui/heading";
 import { Input } from "@/components/ui/input";
 import { Separator } from "@/components/ui/separator";
+import { useOrigin } from "@/hooks/use-origin";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Store } from "@prisma/client";
 import axios from "axios";
@@ -38,6 +39,7 @@ const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
 
   const params = useParams();
   const router = useRouter();
+  const origin = useOrigin();
 
   // 1. Define your form.
   const form = useForm<z.infer<typeof formSchema>>({
